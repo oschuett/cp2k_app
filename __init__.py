a60b8108bdbb9fdbeed4169e424c17216f7deb56
Ole Schuett <ole.schuett@empa.ch>
2017-05-26 17:33:15 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Some package meta-data
+
+def get_app_notebooks():
+    return [ {'name':'CP2K DFT energy calculation', 'notebook':'cp2k_dft_calculation.ipynb'},]
+
+#EOF
