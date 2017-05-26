bf3b32158b4ed8e300752cfb472cf0e1db015905
Ole Schuett <ole.schuett@empa.ch>
2017-05-26 18:26:38 +0200

    fix

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@
 # Some package meta-data
 
 def get_app_notebooks():
-    return [ {'name':'CP2K DFT energy calculation', 'notebook':'cp2k_dft_calculation.ipynb'},]
+    return [ {'name':'Submit CP2K DFT calculation', 'notebook':'cp2k_dft_calculation.ipynb'},
+             {'name':'View CP2K results', 'notebook':'view_cp2k_calc.ipynb'},]
 
 #EOF
