bc132e502a275ecc99e5e5929d86142e19d4c793
Ole Schuett <ole.schuett@empa.ch>
2017-05-29 17:51:21 +0200

    add geo opt

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@
 
 def get_app_notebooks():
     return [ {'name':'Submit CP2K DFT calculation', 'notebook':'cp2k_dft_calculation.ipynb'},
+             {'name':'Submit CP2K geometry optimization', 'notebook':'cp2k_geo_opt.ipynb'},
              {'name':'View CP2K results', 'notebook':'view_cp2k_calc.ipynb'},]
 
 #EOF
